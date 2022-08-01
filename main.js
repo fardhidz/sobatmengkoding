b9c97b0056c263debc971bcd4ee114ff456db0d0
nubisub <hendraksm18@mail.ugm.ac.id>
2022-08-01 21:42:57 +0700

    add navbar

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 window.onload = function() {
     console.log("hello world");
 }
+
+window.addEventListener("scroll", (e) => {
+	const nav = document.getElementById("navbar");
+	if (window.pageYOffset > 50) {
+		nav.classList.add("add-shadow");
+	} else {
+		nav.classList.remove("add-shadow");
+	}
+});
