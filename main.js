595de85d726cda07b446561bccb96692b2037d35
nubisub <hendraksm18@mail.ugm.ac.id>
2022-08-01 23:39:39 +0700

    responsive navbar

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 window.onload = function() {
     console.log("hello world");
+
+    document.getElementById("menu-bar").onclick = function() {
+        document.getElementsByClassName("list-menu")[0].classList.toggle("show");
+        document.getElementsByClassName("navbar")[0].classList.toggle("full-height");
+        document.getElementsByClassName("home-menu")[0].classList.toggle("active");
+    }
+    
 }
 
 window.addEventListener("scroll", (e) => {
