9cef310d8643da2aff4a471320b033b2ecc151e5
nubisub <hendraksm18@mail.ugm.ac.id>
2022-08-03 14:27:52 +0700

    fix responsive navbar

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
-window.onload = function() {
-    console.log("hello world");
+window.onload = function () {
+	console.log("hello world");
+	document.getElementById("menuButton").onclick = menuClick;
+};
 
-    document.getElementById("menu-bar").onclick = function() {
-        document.getElementsByClassName("list-menu")[0].classList.toggle("show");
-        document.getElementsByClassName("navbar")[0].classList.toggle("full-height");
-        document.getElementsByClassName("home-menu")[0].classList.toggle("active");
-    }
-    
+// Menu button click event handler
+function menuClick() {
+	document.getElementsByClassName("list-menu")[0].classList.toggle("show");
+	document.getElementsByClassName("navbar")[0].classList.toggle("full-height");
+	document.getElementsByClassName("home-menu")[0].classList.toggle("active");
+	document.body.classList.toggle("remove-scrolling");
+	document.getElementsByClassName("mark")[0].classList.toggle("show");
+	document.getElementsByClassName("mark")[0].classList.toggle("hidden");
+	document.getElementsByClassName("bar")[0].classList.toggle("show");
+	document.getElementsByClassName("bar")[0].classList.toggle("hidden");
 }
 
+// Add shadow to navbar when scrolling
 window.addEventListener("scroll", (e) => {
 	const nav = document.getElementById("navbar");
 	if (window.pageYOffset > 50) {
