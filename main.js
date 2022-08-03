dfdaf013e1f49adea5495686042563ab09e26d60
nym570 <laililili45@gmail.com>
2022-08-03 23:22:09 +0700

    menambahkan bagian what we do

--- main.js
+++ main.js
@@ -24,3 +24,28 @@ window.addEventListener("scroll", (e) => {
 		nav.classList.remove("add-shadow");
 	}
 });
+
+//mengatur modal what we do
+var modal = document.getElementById("myModal");
+var btn1 = document.getElementById("myBtn1");
+var btn2 = document.getElementById("myBtn2");
+var span = document.getElementsByClassName("close")[0];
+
+btn1.onclick = function() {
+  modal.style.display = "block";
+}
+btn2.onclick = function() {
+  modal.style.display = "block";
+}
+
+//membuka modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+//menutup moda;
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
